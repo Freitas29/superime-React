a50c5636101c7e78338b863111b522c505235e61
Freitas29 <arao.freitas@hotmail.com>
2019-07-30 22:31:12 -0300

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => 
+    <header>
+        <div className="logo">
+            Superime
+        </div>
+
+        <div className="links">
+            <button>
+                Registrar-se
+            </button>
+
+            <button>
+                Entrar
+            </button>
+        </div>
+    </header>
+
+export default Header
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from './components/Header/Header'
 
 const Routes = () => (
     <Router>
         <Switch>
+            <Header />
             <Route exact path="/" component={() => <h1>Hello world</h1>} />
         </Switch>
     </Router>
 )
+
+export default Routes
