92d5401270afac17d849729f6404cb95e27787a3
Freitas29 <arao.freitas@hotmail.com>
2019-08-02 00:12:49 -0300

    Button to search added

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Input from '../../components/Input/Input'
 import Card from '../../components/Card/Card'
+import Button from '../../components/Button/Button'
+import Loading from '../../components/Loading/Loading'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { changeValue,fetchAnime } from '../../redux/Input/SearchFieldActions'
@@ -20,22 +22,13 @@ class Main extends Component{
             <div className="Main">
                 <div className="form-group">
                     <Input holder="Procure por seu anime!" value={this.props.value} onChange={this.props.fetchAnime}/>
+                    <Button value="Buscar"/>
                 </div>
-                {this.props.loading && <p>Carregando</p>}
+
+                {this.props.loading && <Loading />}
                
                 <div className="result-list">
                     {animes}
-                    {/* <Card 
-                    image="https://animeshouse.net/wp-content/uploads/2019/04/93482l-250x350.jpg"
-                    title="Kimtesu no yaiba"
-                    desc="Japão, era Taisho. Tanjiro, um bondoso jovem que ganha a vida vendendo carvão,
-                    descobre que sua família foi massacrada por um demônio. E pra piorar, Nezuko,
-                    sua irmã mais nova e única sobrevivente, também foi transformada num demônio.
-                    Arrasado com esta sombria realidade, Tanjiro decide se tornar um matador de 
-                    demônios para fazer sua irmã voltar a ser humana, e para matar o demônio que 
-                    matou sua família. Um triste conto sobre dois irmãos,
-                    onde os destinos dos humanos e dos demônios se entrelaçam, começa agora.
-                    " /> */}
                 </div>
             </div>
     
